dc78074af9a7f7b2a9b7087b0361abec24fb6e73
ShivaSingh <advanceinadvance1@gmail.com>
2026-04-19 10:53:52 +0530

    Question has been added to the moores voting algo

--- MooresVotingAlgo.cpp
+++ MooresVotingAlgo.cpp
@@ -1,3 +1,12 @@
+// QUESTION1: Majority Element (Classic)
+
+// 👉 Given an array, find the element that appears more than n/2 times.
+
+// Example:
+
+// Input: [3, 2, 3]
+// Output: 3
+
 #include <iostream>
 using namespace std;
 int main ()
